d27ce95b4d720741fd0d0b515e108065ee4453ef
sajamoussa <158780200+sajamoussa@users.noreply.github.com>
2024-07-31 10:29:00 +0100

    fifth commi

--- backend/src/main/java/TodolistApplication.java
+++ backend/src/main/java/TodolistApplication.java
@@ -1,4 +1,4 @@
-package todo.todolist;
+package todo.todolist ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/todo/todolist/TodolistApplicationTests.java
+++ backend/src/test/java/todo/todolist/TodolistApplicationTests.java
@@ -3,7 +3,7 @@ package todo.todolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TodolistApplication.class)
 class TodolistApplicationTests {
 
 	@Test
