1c616ff5b9dcce27357b08214436472337d2ac9e
sajaa45 <moussa.saja@gmail.com>
2024-08-02 14:29:32 +0100

    Test commit for new account

--- backend/src/main/java/model/User.java
+++ backend/src/main/java/model/User.java
@@ -1,16 +1,23 @@
 package model;
-import javax.persistence.*;
 
 import java.io.Serializable;/*let classes be connected */
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Entity/*class is an entity and should be put in a database */
-@AllArgsConstructor/*writes constructors for you without getters setters */
-@NoArgsConstructor/*writes the constructors without args */
-@Data/*uses many annotations in once such as @Getters and @Setters */
-public class User implements Serializable{
+
+@Entity /* class is an entity and should be put in a database */
+@AllArgsConstructor /* writes constructors for you without getters setters */
+@NoArgsConstructor /* writes the constructors without args */
+@Data /* uses many annotations in once such as @Getters and @Setters */
+public class User implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
@@ -20,8 +27,8 @@ public class User implements Serializable{
     private String userName;
     private String password;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    /*user has a one to many rp with categories */
-    /*mappedBy: user owns the rp */
+    /* user has a one to many rp with categories */
+    /* mappedBy: user owns the rp */
     private List<Category> category;
 }
-
+/* kkk */
